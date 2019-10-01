62a758fea77d7db442f53c5fec4bce0cca9322da
uchrissd <uchrissd@protonmail.com>
2019-10-01 12:01:13 -0700

    added index html file to project repor, updated script

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,17 @@
 // This JavaScript program is designed to generate a password for a user and then allow them to copy the generated password to their clipboard.
 // The user has to select at least one of four password criteria options in order for the password to generate.
 
-var specialCharacters = "!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//Possible password values
+var specialCharacters = " !#$%&()*+,-./:;<=>?@[]^_`{|}~";
 var numerals = "1234567890";
-var upperCase = "abcdefghijklmnopqrstuvwxyz";
-var lowerCase = "ABCDEFGHIJKLMNOPQRSTUVYXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVYXYZ";
 
+//Generated password
+var password = "";
+
+//Create a fucntion that sets total character limit for the pasword
+
+//Use conditionals to run through possible user combinations for the password
+
+//Run a loop to determine the undefined password variable
