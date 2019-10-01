ddd841d766d6ff7e5d751a41828a0c11b0d0103d
uchrissd <uchrissd@protonmail.com>
2019-10-01 09:41:58 -0700

    updated js

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+// This JavaScript program is designed to generate a password for a user and then allow them to copy the generated password to their clipboard.
+// The user has to select at least one of four password criteria options in order for the password to generate.
+
+var specialCharacters = "!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var numerals = "1234567890";
+var upperCase = "abcdefghijklmnopqrstuvwxyz";
+var lowerCase = "ABCDEFGHIJKLMNOPQRSTUVYXYZ";
+
