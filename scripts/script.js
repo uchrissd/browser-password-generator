780db5ac9b5faa14296c145de1975c6d2f13db89
uchrissd <uchrissd@protonmail.com>
2019-10-02 16:14:57 -0700

    created some functions

--- scripts/script.js
+++ scripts/script.js
@@ -2,31 +2,54 @@
 // The user has to select at least one of four password criteria options in order for the password to generate.
 
 //Possible password values
+var passwordCharacters = {
+  lowerCase: "abcdefghijklmnopqrstuvwxyz",
+  upperCase: "ABCDEFGHIJKLMNOPQRSTUVYXYZ",
+  numerals: "1234567890",
+  special: "!#$%&()*+,-./:;<=>?@[]^_`{|}~ "
+};
 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVYXYZ";
-var numerals = "1234567890";
-var special = " !#$%&()*+,-./:;<=>?@[]^_`{|}~";
+console.log(
+  "special character string length: ",
+  possiblePassVals.special.length
+);
 
-//Variable length set between range of 9 and 128
-var length = Math.round(Math.random() * (128 - 8) + 8);
-console.log(length);
+//This function sets the password length required to be between 8 and 128
 
-//Generated password
-var password = "";
-
-// User selects criteria, at least one, for password
-
-function generatePassword() {
-    for (var i = 0; x < )
-
-
-
-
+function isValidPasswordLength(passwordLength) {
+  return (
+    isNaN(passwordLength) === false &&
+    passwordLength > 8 &&
+    passwordLength < 128
+  );
 }
 
-//Use conditionals to run through possible user combinations for the password
+//This function validates the password length and prompts the user to enter a desired length
+//Between 8 and 128
 
-//Run a loop to determine the undefined password variable
+function getPasswordLength() {
+  let passwordLength = 0;
+  let question = "Password length? Valid values are between 8 and 128.";
+  while (isValidPasswordLength(passwordLength) === false) {
+    passwordLength = parseInt(prompt(question));
+    question = "Uh oh! Try again.";
+  }
+  return passwordLength;
+}
 
-//Copy password to clipoard
+//Propt user with password generator alert window
+//Prompt user to declare a password length between 8 and 128 characters
+//Prompt user to decide yes or no for lowercase characters
+//Prompt user to decide yes or no for uppercase characters
+//Prompt user to decide yes or no for numbers
+//Prompt user to decide yes or no for special characters
+
+//When user declares desired length of password, store password length to a variable.
+//Save user criteria, all of which is pre-stored in an object, and add the valid characters to an array through a
+//funciton using if else conditionals to validate if the criteria is true or not.
+// For the length of the array of valid choices, run a loop for the number of the desired pw length and
+//Generate a random number every loop that corresponds to an index from the array and append that character to
+//Another array to generate the password..
+//Validate the password
+//Deliver the password to the user
+//Allow the user to copy the password to their clipboard
